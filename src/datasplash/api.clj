efeda40228f91d41d4596d23315d15f6202c6cab
Nils Grunwald <nils@grunwald.fr>
2015-06-12 11:09:35 +0200

    clean up of API namespace code

--- src/datasplash/api.clj
+++ src/datasplash/api.clj
@@ -6,16 +6,22 @@
              [core :as dt]
              [bq :as bq]]))
 
-(def make-transit-coder dt/make-transit-coder)
-(def generate-input dt/generate-input)
+;;;;;;;;;;;;
+;; Coders ;;
+;;;;;;;;;;;;
+
+(def make-nippy-coder dt/make-transit-coder)
+
+;;;;;;;;;;;;;;;
+;; Operators ;;
+;;;;;;;;;;;;;;;
+
 (def view dt/view)
 (def pardo dt/pardo)
 (def identity dt/didentity)
 (def map dt/dmap)
 (def filter dt/dfilter)
 (def mapcat dt/dmapcat)
-(def to-edn dt/to-edn)
-(def from-edn dt/from-edn)
 (def with-keys dt/with-keys)
 (def group-by-key dt/group-by-key)
 (def group-by dt/dgroup-by)
@@ -31,6 +37,11 @@
 (def combine-by dt/combine-by)
 (def combine-fn dt/combine-fn)
 (def juxt dt/djuxt)
+
+;;;;;;;;;;;;;;;;;
+;; Combinators ;;
+;;;;;;;;;;;;;;;;;
+
 (def sum dt/sum)
 (def max dt/dmax)
 (def min dt/dmin)
@@ -44,3 +55,11 @@
 (def read-edn-file dt/read-edn-file)
 (def write-text-file dt/write-text-file)
 (def write-edn-file dt/write-edn-file)
+(def generate-input dt/generate-input)
+
+;;;;;;;;;;;;;
+;; Formats ;;
+;;;;;;;;;;;;;
+
+(def to-edn dt/to-edn)
+(def from-edn dt/from-edn)
