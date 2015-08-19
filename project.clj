001ac55e4a050b50002be73f2773c18026bc303a
Nils Grunwald <nils@grunwald.fr>
2015-08-19 18:00:04 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject datasplash "0.1.0-SNAPSHOT"
+(defproject datasplash "0.2.0-SNAPSHOT"
   :description "Clojure API for a more dynamic Google Dataflow"
   :url "https://github.com/ngrunwald/datasplash"
   :license {:name "Eclipse Public License"
