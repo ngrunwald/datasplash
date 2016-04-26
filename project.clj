bd02219d0242983d7145a7a5aa855380f0e02a5b
Nils Grunwald <nils@grunwald.fr>
2016-04-26 11:00:11 +0200

    release 0.2.0

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject datasplash "0.2.0-SNAPSHOT"
+(defproject datasplash "0.2.0"
   :description "Clojure API for a more dynamic Google Dataflow"
   :url "https://github.com/ngrunwald/datasplash"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[cheshire "5.5.0"]
                  [clj-stacktrace "0.2.8"]
-                 [com.google.cloud.dataflow/google-cloud-dataflow-java-sdk-all "1.4.0"]
+                 [com.google.cloud.dataflow/google-cloud-dataflow-java-sdk-all "1.3.0"]
                  [com.taoensso/nippy "2.10.0"]
                  [org.clojure/clojure "1.7.0"]
                  [org.clojure/math.combinatorics "0.1.1"]
