17737ce408d830924cc190232e04943a011c824a
Nils <nils@grunwald.fr>
2016-07-03 00:49:25 +0200

    bump dependencies

--- project.clj
+++ project.clj
@@ -3,18 +3,18 @@
   :url "https://github.com/ngrunwald/datasplash"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cheshire "5.5.0"]
+  :dependencies [[cheshire "5.6.3"]
                  [clj-stacktrace "0.2.8"]
-                 [com.google.cloud.dataflow/google-cloud-dataflow-java-sdk-all "1.4.0"]
-                 [com.taoensso/nippy "2.10.0"]
-                 [org.clojure/clojure "1.7.0"]
-                 [org.clojure/math.combinatorics "0.1.1"]
+                 [com.google.cloud.dataflow/google-cloud-dataflow-java-sdk-all "1.6.0"]
+                 [com.taoensso/nippy "2.11.1"]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/math.combinatorics "0.1.3"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [clj-time "0.11.0"]
+                 [clj-time "0.12.0"]
                  [superstring "2.1.0"]
                  ;; exclude these if you want something else
-                 [org.slf4j/slf4j-api "1.7.7"]
-                 [org.slf4j/slf4j-jdk14 "1.7.7"]]
+                 [org.slf4j/slf4j-api "1.7.21"]
+                 [org.slf4j/slf4j-jdk14 "1.7.21"]]
   :profiles {:dev {:dependencies [[junit/junit "4.12"]
                                   [me.raynes/fs "1.4.6"]
                                   [org.hamcrest/hamcrest-all "1.3"]]
