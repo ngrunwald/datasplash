e39eb8adff920a1094aeaa322cce24a8062deb81
Nils <nils@grunwald.fr>
2016-10-19 19:17:19 +0200

    update deps (sdk to 1.8.0)

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[cheshire "5.6.3"]
                  [clj-stacktrace "0.2.8"]
-                 [com.google.cloud.dataflow/google-cloud-dataflow-java-sdk-all "1.6.1"]
-                 [com.taoensso/nippy "2.12.1"]
+                 [com.google.cloud.dataflow/google-cloud-dataflow-java-sdk-all "1.8.0"]
+                 [com.taoensso/nippy "2.12.2"]
                  [org.clojure/clojure "1.8.0"]
                  [org.clojure/math.combinatorics "0.1.3"]
                  [org.clojure/tools.logging "0.3.1"]
