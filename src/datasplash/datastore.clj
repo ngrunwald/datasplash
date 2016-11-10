5b3ac55161fac4b8030c5444f3a076ac35081857
Nils <nils@grunwald.fr>
2016-11-10 13:44:44 +0100

    almost working with repeated fields

--- src/datasplash/datastore.clj
+++ src/datasplash/datastore.clj
@@ -6,7 +6,8 @@
    ;; [com.google.cloud.datastore Datastore DatastoreOptions Entity Key KeyFactory]
    [com.google.cloud.dataflow.sdk.io.datastore DatastoreIO ]
    [com.google.cloud.dataflow.sdk Pipeline]
-   [com.google.cloud.dataflow.sdk.values PBegin PCollection])
+   [com.google.cloud.dataflow.sdk.values PBegin PCollection]
+   [com.google.datastore.v1 Entity$Builder Key$Builder Value$Builder])
   (:gen-class))
 
 (defn write-datastore-raw
@@ -29,19 +30,46 @@
                    (cond-> query (.withQuery query)))]
     (apply-transform pcoll ptrans named-schema opts)))
 
+(defprotocol IValDS
+  (make-ds-value-builder [options v]))
+
+(declare make-ds-value)
+
+(extend-protocol IValDS
+  String
+  (make-ds-value-builder [_ ^String v] (DatastoreHelper/makeValue v))
+  java.util.Date
+  (make-ds-value-builder [_ ^java.util.Date v] (DatastoreHelper/makeValue v))
+  Long
+  (make-ds-value-builder [_ ^Long v] (DatastoreHelper/makeValue v))
+  java.util.List
+  (make-ds-value-builder [options ^Iterable v] (mapv #(make-ds-value options %) v))
+  Object
+  (make-ds-value-builder [_ v] (DatastoreHelper/makeValue v)))
+
+(defn make-ds-value
+  [options v]
+  ;; stop building here when print bug id fixed in gcloud and handle excluded afterwards
+  (.build ^Value$Builder (make-ds-value-builder options v)))
+
+(defn make-ds-entity-builder
+  "Generate a datastore entity. Take as parameters the string key of the entity, a map containing the different fields and an options map defining the datastore namespace and kind"
+  [raw-key raw-values {:keys [ds-namespace ds-kind exclude-from-index] :as options}]
+  (let [^Key$Builder key-builder (DatastoreHelper/makeKey (into-array [ds-kind raw-key]))
+        excluded-set (into #{} (map name exclude-from-index))
+        ^Entity$Builder entity-builder (Entity/newBuilder)]
+    (when ds-namespace (.setNamespaceId (.getPartitionIdBuilder key-builder) ds-namespace))
+    (.setKey entity-builder (.build key-builder))
+    (doseq [[v-key v-val] raw-values]
+      (.put (.getMutableProperties entity-builder)
+            (if (keyword? v-key) (name v-key) v-key)
+            (let [^Value$Builder val-builder (make-ds-value-builder options v-val)]
+              (-> val-builder
+                  (cond-> (excluded-set (name v-key)) (.setExcludeFromIndexes true))
+                  (.build)))))
+    entity-builder))
 
 (defn make-ds-entity
-  "Generate a datastore entity. Take as parameters the string key of the entity, a map containing the different fields and an options map defining the datastore namespace and kind"
-  [raw-key raw-values {:keys [ds-namespace ds-kind excluded-from-index] :as options}]
-  (let [key-builder (DatastoreHelper/makeKey (into-array [ds-kind raw-key]))
-        excluded-set (into #{} (map name excluded-from-index))
-        _ (when ds-namespace (.setNamespaceId (.getPartitionIdBuilder key-builder) ds-namespace))
-        entity-builder (Entity/newBuilder)
-        _ (.setKey entity-builder (.build key-builder))
-        _ (doseq [[v-key v-val] raw-values]
-            (.put (.getMutableProperties entity-builder)
-                  (if (keyword? v-key) (name v-key) v-key )
-                  (-> (DatastoreHelper/makeValue v-val)
-                      (cond-> (excluded-set (name v-key)) (.setExcludeFromIndexes true))
-                      (.build ))))]
-    (.build entity-builder)))
+  [raw-key raw-values {:keys [ds-namespace ds-kind exclude-from-index] :as options}]
+  (-> ^Entity$Builder (make-ds-entity-builder raw-key raw-values options)
+      (.build)))
