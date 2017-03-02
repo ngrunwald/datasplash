6fad90178bb3f21c2a99a9225595aaa173c0bc9f
Torbjørn Vatn <torbjorn.vatn@unacast.com>
2017-03-02 13:55:22 +0100

    Bumped some dependencies using the lein ancient plugin

--- project.clj
+++ project.clj
@@ -3,20 +3,20 @@
   :url "https://github.com/ngrunwald/datasplash"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cheshire "5.6.3"]
+  :dependencies [[cheshire "5.7.0"]
                  [clj-stacktrace "0.2.8"]
                  [com.google.cloud.dataflow/google-cloud-dataflow-java-sdk-all "1.9.0"]
-                 [com.taoensso/nippy "2.12.2"]
+                 [com.taoensso/nippy "2.13.0"]
                  [org.clojure/clojure "1.8.0"]
-                 [org.clojure/math.combinatorics "0.1.3"]
+                 [org.clojure/math.combinatorics "0.1.4"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [clj-time "0.12.2"]
+                 [clj-time "0.13.0"]
                  [superstring "2.1.0"]]
   :profiles {:dev {:dependencies [[junit/junit "4.12"]
                                   [me.raynes/fs "1.4.6"]
                                   [org.hamcrest/hamcrest-all "1.3"]
-                                  [org.slf4j/slf4j-api "1.7.21"]
-                                  [org.slf4j/slf4j-jdk14 "1.7.21"]]
+                                  [org.slf4j/slf4j-api "1.7.24"]
+                                  [org.slf4j/slf4j-jdk14 "1.7.24"]]
                    :aot [clojure.tools.logging.impl datasplash.api-test datasplash.examples clj-time.core]
                    :codox {:namespaces [datasplash.api datasplash.bq datasplash.datastore datasplash.pubsub]
                            :source-uri "https://github.com/ngrunwald/datasplash/blob/devel/{filepath}#L{line}"
