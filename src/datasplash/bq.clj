c92477f1ffc457164b4cd6cfb6948ee8d73097c7
Michael Laroche <michael.laroche@oscaro.com>
2017-03-08 15:33:49 +0100

    nested bq row works

--- src/datasplash/bq.clj
+++ src/datasplash/bq.clj
@@ -44,11 +44,11 @@
       (reduce
        (fn [acc k]
          (assoc! acc (keyword k)
-                 (let [raw-v (.get row k)]
+                 (let [raw-v (get row k)]
                    (cond
-                     (sequential? raw-v) (if (instance? java.util.AbstractMap (first raw-v))
-                                           (map #(table-row->clj {:auto-parse auto-parse} %) raw-v)
-                                           (map #(if auto-parse (auto-parse-val %) %) raw-v))
+                     (instance? java.util.List raw-v) (if (instance? java.util.AbstractMap (first raw-v))
+                                                        (map #(table-row->clj {:auto-parse auto-parse} %) raw-v)
+                                                        (map #(if auto-parse (auto-parse-val %) %) raw-v))
                      (instance? java.util.AbstractMap raw-v) (table-row->clj {:auto-parse auto-parse} raw-v)
                      :else (if auto-parse (auto-parse-val raw-v) raw-v)))))
        (transient {}) keyset))))
