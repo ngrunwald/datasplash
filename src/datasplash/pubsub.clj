55747ba13acefb836dd15d662af201eac82ce4ae
Nils Grunwald <nils@grunwald.fr>
2017-03-08 17:35:15 +0100

    add a :kind key to pubsub

--- src/datasplash/pubsub.clj
+++ src/datasplash/pubsub.clj
@@ -5,12 +5,14 @@
            (com.google.cloud.dataflow.sdk Pipeline)))
 
 (defn read-from-pubsub
-  "Create an unbounded PCollection from a pubsub stream"
-  [subscription-or-topic options p]
-  (cond-> p
-          (instance? Pipeline p) (PBegin/in)
-          (re-find #"subscriptions" subscription-or-topic) (apply-transform (PubsubIO$Read/subscription subscription-or-topic) {} options)
-          (re-find #"topics" subscription-or-topic) (apply-transform (PubsubIO$Read/topic subscription-or-topic) {} options)))
+  "Create an unbounded PCollection from a pubsub stream. Takes a :kind option that specifies if the input is a :subscription or a :topic"
+  [subscription-or-topic {:keys [kind] :or {:kind :subscription} :as options} p]
+  (let [pipe (if (instance? Pipeline p) (PBegin/in p) p)]
+    (cond
+      (= :subscription kind) (apply-transform pipe (PubsubIO$Read/subscription subscription-or-topic) {} options)
+      (= :topic kind) (apply-transform pipe (PubsubIO$Read/topic subscription-or-topic) {} options)
+      :else (throw (ex-info (format "Wrong type of :kind for pubsub [%s], should be either :subscription or :topic" kind)
+                            {:kind kind})))))
 
 (defn write-to-pubsub
   "Write the contents of an unbounded PCollection to to a pubsub stream"
