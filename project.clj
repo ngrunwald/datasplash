e8fbd21a7ac23f5839ae74b7f8bef57e375d6cab
Nils <nils@grunwald.fr>
2017-10-05 23:18:31 +0200

    release v0.5.1

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
-(defproject datasplash "0.5.1-SNAPSHOT"
-
+(defproject datasplash "0.5.1"
   :description "Clojure API for a more dynamic Google Cloud Dataflow"
   :url "https://github.com/ngrunwald/datasplash"
   :license {:name "Eclipse Public License"
