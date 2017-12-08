e8f1c95287e721efac93691fb9dc817ea75ddc4c
Nils Grunwald <nils@grunwald.fr>
2017-12-08 18:05:26 +0100

    fix aot bug

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/ngrunwald/datasplash"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cheshire "5.8.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [cheshire "5.8.0"]
                  [clj-stacktrace "0.2.8"]
                  [com.google.cloud.dataflow/google-cloud-dataflow-java-sdk-all "2.1.0"]
                  [com.taoensso/nippy "2.13.0"]
-                 [org.clojure/clojure "1.8.0"]
                  [org.clojure/math.combinatorics "0.1.4"]
                  [org.clojure/tools.logging "0.3.1"]
                  [clj-time "0.13.0"]
@@ -20,7 +20,7 @@
                                   [org.hamcrest/hamcrest-all "1.3"]
                                   [org.slf4j/slf4j-api "1.7.21"]
                                   [org.slf4j/slf4j-jdk14 "1.7.21"]]
-                   :aot  [clojure.tools.logging.impl datasplash.api-test datasplash.examples clj-time.core datasplash.core]
+                   :aot  [clojure.tools.logging.impl datasplash.api-test datasplash.examples clj-time.core datasplash.core clojure.tools.reader.reader-types]
                    :codox {:namespaces [datasplash.api datasplash.bq datasplash.datastore datasplash.pubsub]
                            :source-uri "https://github.com/ngrunwald/datasplash/blob/v0.5.1/{filepath}#L{line}"
                            :metadata {:doc/format :markdown}}
