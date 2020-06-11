4c1b935f14f7f84d4279db2d865b4402f431785f
Dominic Parry <dom.parry@gmail.com>
2020-06-11 11:28:20 +0200

    feat: add :schema-update-options for BQ write

--- src/clj/datasplash/core.clj
+++ src/clj/datasplash/core.clj
@@ -34,8 +34,7 @@
            [org.apache.beam.sdk.transforms.windowing BoundedWindow Window FixedWindows SlidingWindows Sessions Trigger]
            [org.joda.time Duration Instant]
            [datasplash.fns ClojureDoFn ClojureCombineFn ClojureCustomCoder ClojurePTransform]
-           [datasplash.pipelines PipelineWithOptions]
-           ))
+           [datasplash.pipelines PipelineWithOptions]))
 
 (def required-ns (atom #{}))
 
@@ -249,9 +248,9 @@ See https://cloud.google.com/dataflow/java-sdk/JavaDoc/com/google/cloud/dataflow
   [^DoFn$ProcessContext c]
   (let [element (.element c)]
     (if *coerce-to-clj*
-      ( if (instance? KV element)
-       (kv->clj element)
-       element)
+      (if (instance? KV element)
+        (kv->clj element)
+        element)
       element)))
 
 (defrecord MultiResult [kvs])
@@ -481,6 +480,18 @@ See https://cloud.google.com/dataflow/java-sdk/JavaDoc/com/google/cloud/dataflow
                   {:expected (keys enum-map)
                    :given kw}))))))
 
+(defn select-enum-option-fn-set
+  [options enum-map action]
+  (fn [transform options-list]
+    (let [enums (set (vals (select-keys enum-map options-list)))]
+      (if (seq enums)
+        (action transform enums)
+        (throw
+         (ex-info (format "%s must be list of at least one of %s, %s given"
+                          options (keys enum-map) options-list)
+                  {:expected [(keys enum-map)]
+                   :given options-list}))))))
+
 (def base-schema
   {:coder {:docstr "Uses a specific Coder for the results of this transform. Usually defaults to some form of nippy-coder."}
    :checkpoint {:docstr "Given a path, will store the resulting pcoll at this path in edn to facilitate dev/debug."}})
@@ -612,10 +623,10 @@ Example:
    :added "0.1.0"}
   ([coll options ^Pipeline p]
    (let [{:keys [coder] :as opts} (merge {:coder (make-nippy-coder)}
-                                (assoc options :label :generate-input))
+                                         (assoc options :label :generate-input))
          ptrans (if (empty? coll)
-                 (Create/empty coder)
-                 (Create/of coll))]
+                  (Create/empty coder)
+                  (Create/of coll))]
      (apply-transform p ptrans base-schema opts)))
   ([coll p] (generate-input coll {} p))
   ([p] (generate-input [] {} p)))
@@ -662,12 +673,12 @@ This function is reminiscent of the reducers api. In has sensible defaults in or
          output-coder (or output-coder (make-nippy-coder))
          acc-coder (or acc-coder (make-nippy-coder))
 
-         init-fn (fn [] (safe-exec (initf )))
+         init-fn (fn [] (safe-exec (initf)))
          reduce-fn (fn [acc elt] (safe-exec (reducef acc elt)))
          combine-fn (fn [accs] (safe-exec (apply combinef accs)))
          extract-fn (fn [acc] (safe-exec (extractf acc)))]
      (ClojureCombineFn. {"init-fn" init-fn "reduce-fn" reduce-fn "combine-fn" combine-fn
-                         "combine-fn-raw" combinef "extract-fn" extract-fn }
+                         "combine-fn-raw" combinef "extract-fn" extract-fn}
                         output-coder acc-coder)))
 
   ([reducef extractf combinef initf output-coder] (combine-fn reducef extractf combinef initf output-coder nil))
@@ -796,7 +807,7 @@ Example:
               cfs)))
      (fn []
        (mapv (fn [cf] (let [f (.getInitFn cf)]
-                       (f))) cfs))
+                        (f))) cfs))
      (.getDefaultOutputCoder (first cfs) nil nil)
      (.getAccumulatorCoder (first cfs) nil nil))))
 
@@ -837,7 +848,6 @@ See https://cloud.google.com/dataflow/java-sdk/JavaDoc/com/google/cloud/dataflow
          opts (assoc options :label :group-by-keys)]
      (apply-transform pcoll (GroupByKey/create) base-schema opts)))
   ([pcoll] (group-by-key {} pcoll)))
-
 
 (defmacro ptransform
   {:doc "Generates a PTransform with the given name, apply signature and body. Should rarely by used in user code, see [[pt->>]] for the more general use case in application code.
@@ -1052,9 +1062,9 @@ It means the template %A-%U-%T is equivalent to the default jobName"
 
 (defn split-path
   [p]
-  (let[[_ base-path filename :as all] (->> (re-find #"^(.*/)([^/]*)$" p )
-                                           (remove #(= "" %)))
-       filename (or filename (when (empty? all) p))]
+  (let [[_ base-path filename :as all] (->> (re-find #"^(.*/)([^/]*)$" p)
+                                            (remove #(= "" %)))
+        filename (or filename (when (empty? all) p))]
     [base-path filename]))
 
 (defn ->options
@@ -1086,7 +1096,7 @@ It means the template %A-%U-%T is equivalent to the default jobName"
                                     empty-match-treatment-enum
                                     (fn [transform enum] (.withEmptyMatchTreatment transform enum)))}
    :delimiter {:docstr "Specify delimiter"
-                :action (fn [transform delimiter] (.withDelimiter transform delimiter))}
+               :action (fn [transform delimiter] (.withDelimiter transform delimiter))}
    :compression-type {:docstr "Choose compression type. :auto by default."
                       :enum compression-type-enum
                       :action (select-enum-option-fn
@@ -1104,7 +1114,6 @@ It means the template %A-%U-%T is equivalent to the default jobName"
                                                     :inactivity (Watch$Growth/afterTimeSinceNewOutput
                                                                  (->duration termination-duration))
                                                     termination-strategy)))}})
-
 
 (def text-writer-schema
   {:file-format {:docstr "Choose file format."
@@ -1139,11 +1148,8 @@ It means the template %A-%U-%T is equivalent to the default jobName"
    :prefix {:docstr "Uses the given filename prefix."
             :action (fn [transform suffix] (.withPrefix transform suffix))}
    :naming-fn {:docstr "Uses the naming fn"
-               :action (fn [transform naming-fn] (.withNaming transform (sfn naming-fn) ))}
-   :dynamic-fn {:docstr "Uses the dynamic write to change destination file according to the content of the item"
-                }})
-
-
+               :action (fn [transform naming-fn] (.withNaming transform (sfn naming-fn)))}
+   :dynamic-fn {:docstr "Uses the dynamic write to change destination file according to the content of the item"}})
 
 (defn write-text-file
   {:doc (with-opts-docstr
@@ -1174,10 +1180,6 @@ See https://cloud.google.com/dataflow/java-sdk/JavaDoc/com/google/cloud/dataflow
                          (.to (or base-path "./")))
                      (merge named-schema text-writer-schema) opts)))
 
-
-
-
-
 (defn read-text-file
   {:doc (with-opts-docstr "Reads a PCollection of Strings from disk or Google Storage, with records separated by newlines.
 
@@ -1197,10 +1199,8 @@ Example:
      (-> p
          (cond-> (instance? Pipeline p) (PBegin/in))
          (apply-transform (.from (TextIO/read) from)
-                          (merge named-schema text-reader-schema) opts)))
-   )
+                          (merge named-schema text-reader-schema) opts))))
   ([from p] (read-text-file from {} p)))
-
 
 (defn read-text-files
   {:doc (with-opts-docstr "Reads multiple text files from a PCollection of Strings from disk or Google Storage, with records separated by newlines.
@@ -1214,21 +1214,20 @@ Example:
 ```")}
   ([options ^PCollection from]
    (let [opts (assoc options
-                :coder (or
-                         (:coder options)
-                         (StringUtf8Coder/of)))
+                     :coder (or
+                             (:coder options)
+                             (StringUtf8Coder/of)))
          transform (ptransform
-                     (or (:name options) "read-text-files")
-                     [^PCollection pcoll]
-                     (-> pcoll
-                         (apply-transform (FileIO/matchAll)
-                                          {} {})
-                         (apply-transform (FileIO/readMatches)
-                                          {} {})
-                         (apply-transform (TextIO/readFiles)
-                                          (merge named-schema text-reader-schema) (dissoc opts :name))))]
-     (apply-transform from transform (merge {:name "read-text-files"} options) opts)
-     ))
+                    (or (:name options) "read-text-files")
+                    [^PCollection pcoll]
+                    (-> pcoll
+                        (apply-transform (FileIO/matchAll)
+                                         {} {})
+                        (apply-transform (FileIO/readMatches)
+                                         {} {})
+                        (apply-transform (TextIO/readFiles)
+                                         (merge named-schema text-reader-schema) (dissoc opts :name))))]
+     (apply-transform from transform (merge {:name "read-text-files"} options) opts)))
   ([from] (read-text-files {} from)))
 
 (defn read-edn-file
@@ -1253,7 +1252,6 @@ Example:
       (from-edn (assoc options :name "parse-edn")))))
   ([from p] (read-edn-file from {} p)))
 
-
 (defn read-edn-files
   {:doc (with-opts-docstr "Reads multiple EDN files from a PCollection of Strings from disk or Google Storage, with records separated by newlines.
 
@@ -1277,7 +1275,6 @@ Example:
                            (assoc :name "read-edn-files")))
       (from-edn (assoc options :name "parse-edn")))))
   ([from] (read-edn-files {} from)))
-
 
 (defn write-edn-file
   {:doc (with-opts-docstr
@@ -1330,7 +1327,6 @@ Example:
             (assoc options :name "json-decode")))))
   ([from p] (read-json-file from {} p)))
 
-
 (defn read-json-files
   {:doc (with-opts-docstr "Reads multiple JSON files from a PCollection of Strings from disk or Google Storage, with records separated by newlines.
 
@@ -1363,12 +1359,10 @@ Example:
             (assoc options :name "json-decode")))))
   ([from] (read-json-files {} from)))
 
-
 (def json-writer-schema
   {:date-format {:docstr "Pattern for encoding java.util.Date objects. Defaults to yyyy-MM-dd'T'HH:mm:ss'Z'"}
    :escape-non-ascii {:docstr "Generate JSON escaping UTF-8."}
    :key-fn {:docstr "Generate JSON and munge keys with a custom function."}})
-
 
 (defn write-json-file
   {:doc (with-opts-docstr
@@ -1629,7 +1623,7 @@ See https://cloud.google.com/dataflow/java-sdk/JavaDoc/com/google/cloud/dataflow
                        res (map (fn [prod] (apply clean-join-fn prod)) raw-res)]
                    res))
                {:name (str root-name "-cartesian-product")
-                :without-coercion-to-clj true }))))
+                :without-coercion-to-clj true}))))
   ([options specs] (if (fn? specs)
                      (throw (ex-info "Wrong type of argument for join-by, join-fn is now passed as a :collector key in options" {:specs specs}))
                      (join-by options specs nil))))
@@ -1893,8 +1887,6 @@ Example:
      (apply-transform pcoll (Count/perElement) named-schema opts)))
   ([pcoll] (dfrequencies {} pcoll)))
 
-
-
 (def accumulation-mode-enum
   {:accumulate #(.accumulatingFiredPanes %)
    :discard #(.discardingFiredPanes %)})
@@ -1970,11 +1962,10 @@ Example:
      (apply-transform pcoll transform window-schema options)))
   ([gap ^PCollection pcoll] (session-windows gap {} pcoll)))
 
-
 (defn- mk-default-windowed-fn
   [{:keys [file-name suffix] :as options
     :or {file-name "file"}}]
-  (fn [shard-number shard-count ^BoundedWindow window _ ]
+  (fn [shard-number shard-count ^BoundedWindow window _]
     (safe-exec
      (let [timestamp (timf/unparse (:date-hour-minute timf/formatters)
                                    (.start window))]
@@ -2042,7 +2033,3 @@ Examples:
   (let [[expressions clauses] (parse-try body)]
     `(try (safe-exec ~@expressions)
           ~@clauses)))
-
-
-
-
