71e775bee7b74269e37d45561b528ac47c20c158
Ed Porras <dev@digressed.net>
2022-04-09 15:54:37 +0200

    bump dependencies; switch `:dev` to use logback-core

--- project.clj
+++ project.clj
@@ -5,19 +5,19 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/math.combinatorics "0.1.6"]
-                 [org.clojure/tools.logging "1.2.2"]
+                 [org.clojure/tools.logging "1.2.4"]
 
                  [metosin/jsonista "0.3.5"]
                  [clj-stacktrace "0.2.8"]
                  [clj-time "0.15.2"]
                  [com.taoensso/nippy "3.1.1"]
-                 [org.apache.beam/beam-sdks-java-core "2.34.0"]
-                 [org.apache.beam/beam-sdks-java-io-elasticsearch "2.34.0"]
-                 [org.apache.beam/beam-sdks-java-io-kafka "2.34.0"]
-                 [org.apache.beam/beam-runners-direct-java "2.34.0"]
-                 [org.apache.beam/beam-runners-google-cloud-dataflow-java "2.34.0"]
-                 [org.apache.beam/beam-runners-core-java "2.34.0"]
-                 [org.apache.kafka/kafka-clients "3.0.0"]
+                 [org.apache.beam/beam-sdks-java-core "2.37.0"]
+                 [org.apache.beam/beam-sdks-java-io-elasticsearch "2.37.0"]
+                 [org.apache.beam/beam-sdks-java-io-kafka "2.37.0"]
+                 [org.apache.beam/beam-runners-direct-java "2.37.0"]
+                 [org.apache.beam/beam-runners-google-cloud-dataflow-java "2.37.0"]
+                 [org.apache.beam/beam-runners-core-java "2.37.0"]
+                 [org.apache.kafka/kafka-clients "3.1.0"]
                  [superstring "3.1.0"]]
   :source-paths ["src/clj"]
   :pedantic? false
@@ -27,8 +27,7 @@
   :profiles {:dev {:dependencies [[junit/junit "4.13.2"]
                                   [me.raynes/fs "1.4.6"]
                                   [org.hamcrest/hamcrest-all "1.3"]
-                                  [org.slf4j/slf4j-api "1.7.32"]
-                                  [org.slf4j/slf4j-jdk14 "1.7.32"]]
+                                  [ch.qos.logback/logback-core "1.2.11"]]
                    :aot  [clojure.tools.logging.impl datasplash.api-test datasplash.examples clj-time.core datasplash.core clojure.tools.reader.reader-types]}
              :uberjar {:aot :all}}
   :main datasplash.examples)
