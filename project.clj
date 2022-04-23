96901895e0b50693856b9b9279d00c1b95e7187d
Ed Porras <dev@digressed.net>
2022-04-23 11:21:03 +0200

    Version 0.7.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject datasplash "0.7.6"
+(defproject datasplash "0.7.7-SNAPSHOT"
   :description "Clojure API for a more dynamic Google Cloud Dataflow and (hopefully) Apache BEAM"
   :url "https://github.com/ngrunwald/datasplash"
   :license {:name "Eclipse Public License"
